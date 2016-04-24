2aabd2d63565be96125b215e53c515dd4ecf28bf
Michael Aivazis <michael.aivazis@orthologue.com>
2016-04-24 12:10:17 -0700

    group: primed the list of developers

---
+++ group/Make.mm
@@ -0,0 +1,28 @@
+# -*- Makefile -*-
+#
+# michael a.g. aïvázis
+# parasim
+# (c) 2003-2016 all rights reserved
+#
+
+# project defaults
+include isce.def
+# package name
+PACKAGE = group
+# add this to the clean pile
+PROJ_CLEAN += authorized_keys
+# the list of public keys
+PUBLIC_KEYS = $(wildcard *.pub)
+
+# standard targets
+all: tidy
+# make the autorized keys file
+authorized_keys: $(PUBLIC_KEYS) grant.py grant.pfg Make.mm
+	./grant.py
+
+publish: authorized_keys
+	$(SCP) $< $(PROJ_REPO_ADMIN)@$(PROJ_REPO_HOST):$(PROJ_REPO_HOME)/.ssh
+
+live: publish clean
+
+# end of file
