bd8a17a81150b336561cee19dc7f1ea869a80546
Michael Aivazis <michael.aivazis@para-sim.com>
2016-09-18 15:55:42 +0300

    isce: the extension module now reports the {libisce} version

--- extensions/isce/metadata.cc
+++ extensions/isce/metadata.cc
@@ -47,7 +47,16 @@ PyObject *
 isce::extension::
 version(PyObject *, PyObject *)
 {
-        return Py_BuildValue("s", isce::version);
+    // get the version info
+    isce::version_t version { isce::version() };
+    // make a tuple
+    PyObject * result = PyTuple_New(3);
+    // add the three fields
+    PyTuple_SET_ITEM(result, 0, PyLong_FromLong(version[0]));
+    PyTuple_SET_ITEM(result, 1, PyLong_FromLong(version[1]));
+    PyTuple_SET_ITEM(result, 2, PyLong_FromLong(version[2]));
+    // hand back to the caller
+    return result;
 }
 
 
