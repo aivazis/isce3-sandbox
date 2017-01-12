7088df5542b3418ccc1ceb8cc85fcf3717d0bb86
Michael Aivazis <michael.aivazis@para-sim.com>
2017-01-12 21:44:00 +0200

    doc: added the first few slides of the pyre tutorial

--- doc/Make.mm
+++ doc/Make.mm
@@ -8,13 +8,7 @@
 include isce.def
 # my subdirectories
 RECURSE_DIRS = \
-    lib \
-    extensions \
-    packages \
-    etc \
-    bin \
-    doc \
-    tests \
+    pyre \
 
 # the standard targets
 all:
@@ -34,11 +28,5 @@ export::
 
 live:
 	BLD_ACTION="live" $(MM) recurse
-
-pull:
-	bzr pull bzr+ssh://$(PROJECT)@$(PROJ_REPO_HOST)
-
-push:
-	bzr push bzr+ssh://$(PROJECT)@$(PROJ_REPO_HOST)
 
 # end of file
---
+++ doc/pyre/Make.mm
@@ -0,0 +1,48 @@
+# -*- Makefile -*-
+#
+# michael a.g. aïvázis
+# orthologue
+# (c) 1998-2017 all rights reserved
+#
+
+PROJECT = isce
+PACKAGE = doc/pyre
+
+OTHERS = \
+
+DOCUMENT = pyre
+
+PACKAGES =
+
+INCLUDES = \
+    config/*.sty \
+    config/*.tex \
+
+SECTIONS = \
+    sections/*.tex \
+    sections/*.bib \
+
+zLISTINGS = \
+    listings/*.py \
+
+FIGURES = \
+    figures/*.pdf \
+
+#
+all: $(DOCUMENT).pdf
+
+# preview types
+osx: $(DOCUMENT).pdf
+	open $(DOCUMENT).pdf
+
+xpdf: $(DOCUMENT).pdf
+	xpdf -remote $(DOCUMENT) $(DOCUMENT).pdf
+
+# make the document using the default document class
+$(DOCUMENT).pdf: $(DOCUMENT).tex $(PACKAGES) $(INCLUDES) $(SECTIONS) $(LISTINGS) $(FIGURES)
+
+# housekeeping
+PROJ_CLEAN += $(CLEAN_LATEX) *.snm *.nav *.vrb
+PROJ_DISTCLEAN = *.ps *.pdf $(PROJ_CLEAN)
+
+# end of file
